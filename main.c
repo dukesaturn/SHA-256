337b89a0090a1b79e9c8aaea667dc4a5bc3f5bc2
what <labs@dksengine.com>
2026-05-09 08:33:57 +0000

    feat: add dockblocks

--- main.c
+++ main.c
@@ -6,20 +6,29 @@
 #include <stdio.h>
 #include "src/sha-256.h"
 #include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#define PROGRAM "sha256"
 
 int main(int argc, char **argv)
 {
     if (argc != 2)
     {
-        puts("Usage: sha <string>");
+        fprintf(stderr, "Usage: %s <string>\n", PROGRAM);
         return 1;
     }
 
-    const char* input =  argv[1];
+    const char *input = argv[1];
     char result[65] = {0};
 
-    if (!sha256(input,  strlen(input), result))
-        puts((char*)result);
-    
+    if (!sha256(input, strlen(input), result))
+        puts((char *)result);
+    else
+    {
+        fprintf(stderr, "Error with %s: %s\n", PROGRAM, strerror(errno));
+        return 1;
+    }
+
     return 0;
 }
